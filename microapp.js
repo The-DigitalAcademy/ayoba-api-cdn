5a580e3d452eca1695269bf7eb96573d1b326316
simphwe-nkabinde <simphiwe.kabini@gmail.com>
2023-03-01 10:31:56 +0200

    changes

--- microapp.js
+++ microapp.js
@@ -2,6 +2,34 @@ window.onload = () => {
 
     var Ayoba = getAyoba()
 
+        /**
+    * Determine the mobile operating system and returns the
+    * proper javascript interface
+    */
+        function getAyoba() {
+            var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    
+            // Windows Phone must come first because its UA also contains "Android"
+            if (/windows phone/i.test(userAgent)) {
+                return null;
+            }
+    
+            if (/android/i.test(userAgent)) {
+                try {
+                    return Android;
+                } catch (error) {
+                    return null;
+                }
+            }
+    
+            // iOS detection from: http://stackoverflow.com/a/9039885/177710
+            if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+                return null; // todo
+            }
+    
+            return "unknown";
+        }
+
     if (Ayoba == null || Ayoba == 'unknown') {
         //Browser test Environment
         Ayoba = new AyobaStub();
@@ -209,33 +237,6 @@ window.onload = () => {
             console.log(err);
         });
 
-    }
-    /**
-    * Determine the mobile operating system and returns the
-    * proper javascript interface
-    */
-    function getAyoba() {
-        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-        // Windows Phone must come first because its UA also contains "Android"
-        if (/windows phone/i.test(userAgent)) {
-            return null;
-        }
-
-        if (/android/i.test(userAgent)) {
-            try {
-                return Android;
-            } catch (error) {
-                return null;
-            }
-        }
-
-        // iOS detection from: http://stackoverflow.com/a/9039885/177710
-        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-            return null; // todo
-        }
-
-        return "unknown";
     }
 
 }
